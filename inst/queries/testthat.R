432779da344f096e378b4f1ea11a2a227d426e79
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-11-21 13:52:40 +0100

    Added succeed and fail to assertion query

--- inst/queries/testthat.R
+++ inst/queries/testthat.R
@@ -3,5 +3,11 @@ list(
   parts = list(list(
     callName = "expect(_.*)?",
     subkind = "expect"
+  ), list(
+    callName = "fail",
+    subkind = "fail"
+  ), list(
+    callName = "succeed",
+    subkind = "succeed"
   ))
 )
