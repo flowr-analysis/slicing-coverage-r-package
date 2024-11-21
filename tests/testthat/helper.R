e1549ef2dc3938a54393964b4591ec9e0437f2f1
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-11-21 13:52:12 +0100

    Reuse cached file for testing

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,5 +1,12 @@
+cache <- new.env()
+
 file_with_content <- function(content, ext = ".R") {
+  hash <- rlang::hash(list(c = content, e = ext))
+  if (exists(hash, envir = cache)) {
+    return(get(hash, envir = cache))
+  }
   tmp <- tempfile(pattern = "slccov-test-", fileext = ext)
   writeLines(content, tmp)
+  assign(hash, tmp, envir = cache)
   return(tmp)
 }
