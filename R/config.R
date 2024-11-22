edafb0f0c7c77eea8aab2559f99c105ef9f5ca1e
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-11-22 18:21:38 +0100

    Improved configure function

--- R/config.R
+++ R/config.R
@@ -21,38 +21,28 @@ option_env$unknown_locations <- FALSE
 #' @param log_level The minimum level of logs that should be shown
 #' @param unknown_locations Whether to return the number of source refs flowr did not
 #' know about
+#' @param list A list of options to set.
 #'
 #' @export
-configure <- function(flowr_host = NULL,
-                      flowr_port = NULL,
-                      measure_time = NULL,
-                      return_covr_result = NULL,
-                      slicing_points = NULL,
-                      log_level = NULL,
-                      unknown_locations = NULL) {
-  if (!missing(flowr_host)) {
-    option_env$flowr_host <- flowr_host
+configure <- function(list = NULL, ...) {
+  all_opts <- list()
+  for (opt in ls(option_env)) {
+    all_opts[[opt]] <- get_option(opt)
   }
-  if (!missing(flowr_port)) {
-    option_env$flowr_port <- flowr_port
+
+  args <- if (is.list(list)) list else list(...)
+  for (arg in names(args)) {
+    if (!exists(arg, envir = option_env)) {
+      warning(sprintf("Option '%s' does not exist", arg))
+    }
+    option_env[[arg]] <- args[[arg]]
+    if (arg == "log_level") {
+      logger::log_threshold(level = args[[arg]], namespace = "slicingCoverage")
+      logger::log_formatter(logger::formatter_sprintf, namespace = "slicingCoverage")
+    }
   }
-  if (!missing(measure_time)) {
-    option_env$measure_time <- measure_time
-  }
-  if (!missing(return_covr_result)) {
-    option_env$return_covr_result <- return_covr_result
-  }
-  if (!missing(slicing_points)) {
-    option_env$slicing_points <- slicing_points
-  }
-  if (!missing(log_level)) {
-    option_env$log_level <- log_level
-    logger::log_threshold(level = log_level, namespace = "slicingCoverage")
-    logger::log_formatter(logger::formatter_sprintf, namespace = "slicingCoverage")
-  }
-  if (!missing(unknown_locations)) {
-    option_env$unknown_locations <- unknown_locations
-  }
+
+  return(all_opts)
 }
 
 
@@ -63,4 +53,17 @@ configure <- function(flowr_host = NULL,
 #' @export
 get_option <- function(option) {
   return(option_env[[option]])
+}
+
+#' Executes an expression with the given options set and resets them afterwards.
+#'
+#' @param options A list of options to set.
+#' @param expr The expression to execute
+#'
+#' @return The result of the expression.
+#' @export
+with_options <- function(options, expr) {
+  prev_opts <- configure(options)
+  on.exit(configure(list = prev_opts))
+  return(expr)
 }
--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -1,8 +1,21 @@
-test_that("Reading config options set with 'configure' yields the set value", {
-  configure(flowr_host = "test.host", flowr_port = 1234, measure_time = TRUE, return_covr_result = TRUE)
+test_that("with_options correctly resets options after execution", {
+  opts <- configure()
+  with_options(list(flowr_host = "not.localhost", flowr_port = 1234), {
+    expect_equal(get_option("flowr_host"), "not.localhost")
+    expect_equal(get_option("flowr_port"), 1234)
+  })
+  expect_equal(configure(), opts)
+})
 
-  expect_equal(get_option("flowr_host"), "test.host")
-  expect_equal(get_option("flowr_port"), 1234)
-  expect_equal(get_option("return_covr_result"), TRUE)
-  expect_equal(get_option("measure_time"), TRUE)
+test_that("reading config options set with 'configure' yields the set value", {
+  with_options(list(flowr_host = "localhost", flowr_port = 1042, measure_time = FALSE, return_covr_result = FALSE), {
+    expect_equal(get_option("flowr_host"), "localhost")
+    expect_equal(get_option("flowr_port"), 1042)
+    expect_equal(get_option("measure_time"), FALSE)
+    expect_equal(get_option("return_covr_result"), FALSE)
+  })
+})
+
+test_that("setting nonexistent options yields a warning", {
+  expect_warning(configure(nonexistent = "value"))
 })
